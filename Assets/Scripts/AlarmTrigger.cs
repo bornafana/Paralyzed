bc742b972310d6d292a2cfef255b5041b0f8ecdf
bornafana <borna.fana@gmail.com>
2020-02-10 15:01:35 +0400

    Init - second

--- Assets/Scripts/AlarmTrigger.cs
+++ Assets/Scripts/AlarmTrigger.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using ExitGames.Client.Photon;
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
 public class AlarmTrigger : Interactable
 {
+    private const byte WHISTLE = 1;
+
     public AudioSource source;
     public AudioClip clipToPlay;
     public override void DoAction()
     {
-        PhotonNetwork.SetSendingEnabled(2, false);
-        photonView.RPC("PlaySound", RpcTarget.All);
+        //PhotonNetwork.SetSendingEnabled(2, false);
+        object clip = clipToPlay;
+        PhotonNetwork.RaiseEvent(WHISTLE, clip, RaiseEventOptions.Default, SendOptions.SendUnreliable);
     }
 
     [PunRPC]
